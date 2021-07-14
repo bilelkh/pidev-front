8969fea2330542f7506ff5eb321f09b09aeefc52
youssef <ybouhachem@gmail.com>
2021-07-14 16:27:17 +0100

    WIP: add order & coupon crud

---
+++ Controllers/CouponsController.cs
@@ -0,0 +1,124 @@
+﻿using Pidev_front.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pidev_front.Controllers
+{
+    public class CouponsController : Controller
+    {
+        HttpClient httpClient;
+
+        public CouponsController()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:8080/coupon/");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        // GET: Coupon
+        public ActionResult Index()
+        {
+            var response = httpClient.GetAsync("all").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var categories = response.Content.ReadAsAsync<IList<Coupon>>().Result;
+                return View(categories);
+            }
+            else
+            {
+                return View(new List<Coupon>());
+            }
+        }
+
+        // GET: Coupon/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Coupon/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Coupon/Create
+        [HttpPost]
+        public ActionResult Create(Coupon coupon)
+        {
+            try
+            {
+                coupon.Id = null;
+
+                var response = httpClient.PostAsJsonAsync("", coupon).ContinueWith(task =>
+                {
+                    if (task.Result.IsSuccessStatusCode)
+                    {
+                        var result = task.Result.Content.ReadAsStringAsync();
+                    }
+                    else
+                    {
+                        task.Result.EnsureSuccessStatusCode();
+                    }
+                });
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Coupon/Edit/5
+        public ActionResult Edit(string CouponCode)
+        {
+            if (CouponCode is null)
+            {
+                throw new ArgumentNullException(nameof(CouponCode));
+            }
+
+            var response = httpClient.GetAsync("?coupon=" + CouponCode).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var categories = response.Content.ReadAsAsync<Coupon>().Result;
+                return View(categories);
+            }
+            else
+            {
+                return View(new Coupon());
+            }
+        }
+
+        // POST: Coupon/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Coupon coupon)
+        {
+            try
+            {
+
+                var response = httpClient.PutAsJsonAsync("", coupon).ContinueWith(task =>
+                {
+                    if (task.Result.IsSuccessStatusCode)
+                    {
+                        var result = task.Result.Content.ReadAsStringAsync();
+                    }
+                    else
+                    {
+                        task.Result.EnsureSuccessStatusCode();
+                    }
+                });
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Models/Coupon.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pidev_front.Models
+{
+    public class Coupon
+    {
+        [JsonProperty("id")]
+        public int? Id { get; set; }
+
+        [JsonProperty("couponCode")]
+        public string CouponCode { get; set; }
+
+        [JsonProperty("startDate")]
+        public DateTime StartDate { get; set; }
+
+        [JsonProperty("endDate")]
+        public DateTime EndDate { get; set; }
+
+        [JsonProperty("discount")]
+        public int Discount { get; set; }
+
+        [JsonProperty("isPercentage")]
+        public Boolean IsPercentage { get; set; }
+
+        [JsonProperty("active")]
+        public Boolean Active { get; set; }
+
+        [JsonProperty("type")]
+        public CouponType Type { get; set; }
+
+        [JsonProperty("appliedWithPromo")]
+        public Boolean AppliedWithPromo { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTime? CreatedAt { get; set; }
+
+        [JsonProperty("updatedAt")]
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+
+    public enum CouponType
+    {
+        Global, Unique, UniquePerUser
+    }
+
+}
