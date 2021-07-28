7ee5985c7b2ad756a7bbdb1a12e84dc2f3317703
hmgaidi <hmgaidi@lbconsulting.com.tn>
2021-07-28 12:20:37 +0100

    add : fintecture payment details response model

---
+++ Models/PaymentDetailsResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Pidev_front.Models
+{
+    public class Response
+    {
+        [JsonProperty("finSessionId")]
+        public String FinSessionId { get; set; }
+
+        [JsonProperty("finUrl")]
+        public String FinUrl { get; set; }
+    }
+}
