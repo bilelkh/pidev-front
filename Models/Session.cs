44e5ebbd8982fdb86714af499bd830c2cb323839
hmgaidi <hmgaidi@lbconsulting.com.tn>
2021-07-28 12:18:30 +0100

    add : session model

---
+++ Models/Session.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Pidev_front.Models
+{
+    public class Session
+    {
+
+        [JsonProperty("user")]
+        public User User { get; set; }
+
+        [JsonProperty("bookList")]
+        public List<Book> BookList { get; set; }
+
+        [JsonProperty("url")]
+        public String Url { get; set; }
+
+        [JsonProperty("totalPrice")]
+        public Double TotalPrice { get; set; }
+
+        [JsonProperty("codeCoupon")]
+        public String CodeCoupon { get; set; }
+
+        [JsonProperty("couponIsApplied")]
+        public Boolean CouponIsApplied { get; set; }
+
+    }
+}
